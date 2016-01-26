73fa3633d0fe5761d99c9713995afb8979164f11
Tim Tegeler <tim@tegeler.me>
2016-01-26 16:55:43 +0100

    Added states to holidays

--- src/german-holidays.js
+++ src/german-holidays.js
@@ -22,53 +22,102 @@ angular.module('german-holidays', []).factory('holidayCheck', function () {
         return new Date(y, n - 1, p);
     }
 
+    function bubeCaluclation (date) {
+        // calculates the "Buß- und Bettag"
+        var nov23 = new Date(date.getFullYear(), 10, 23);
+        nov23.setDate(nov23.getDate() - 1);
+        while (true) {
+            if (nov23.getDay() == 3) {
+                return nov23
+            } else {
+                nov23.setDate(nov23.getDate() - 1)
+            }
+        }
+    }
+
     function holidayCheck(date) {
 
         var easter = easterCalculation(date.getFullYear());
         var holidays = [
             {
                 name: 'Neujahrstag',
-                date: new Date(date.getFullYear(), 0, 1)
+                date: new Date(date.getFullYear(), 0, 1),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
+            },
+            {
+                name: 'Heilige drei Könige',
+                date: new Date(date.getFullYear(), 0, 6),
+                states: ['BW', 'BY', 'ST']
             },
             {
                 name: 'Karfreitag',
-                date: new Date(easter.valueOf()).setDate(easter.getDate() - 2)
+                date: new Date(easter.valueOf()).setDate(easter.getDate() - 2),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
+            },
+            {
+                name: 'Ostersonntag',
+                date: new Date(easter.valueOf()),
+                states: ['BB']
             },
             {
                 name: 'Ostermontag',
-                date: new Date(easter.valueOf()).setDate(easter.getDate() + 1)
+                date: new Date(easter.valueOf()).setDate(easter.getDate() + 1),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
             },
             {
                 name: 'Tag der Arbeit',
-                date: new Date(date.getFullYear(), 4, 1)
+                date: new Date(date.getFullYear(), 4, 1),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
             },
             {
                 name: 'Christi Himmelfahrt',
-                date: new Date(easter.valueOf()).setDate(easter.getDate() + 39)
+                date: new Date(easter.valueOf()).setDate(easter.getDate() + 39),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
             },
             {
-                name: 'Pfingsmontag',
-                date: new Date(easter.valueOf()).setDate(easter.getDate() + 50)
+                name: 'Pfingstsonntag',
+                date: new Date(easter.valueOf()).setDate(easter.getDate() + 50),
+                states: ['BB']
+            },
+            {
+                name: 'Pfingstmontag',
+                date: new Date(easter.valueOf()).setDate(easter.getDate() + 50),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
             },
             {
                 name: 'Fronleichnam',
-                date: new Date(easter.valueOf()).setDate(easter.getDate() + 60)
+                date: new Date(easter.valueOf()).setDate(easter.getDate() + 60),
+                states: ['BW', 'BY', 'HE', 'NW', 'RP', 'SL']
             },
             {
                 name: 'Tag der deutschen Einheit',
-                date: new Date(date.getFullYear(), 9, 3)
+                date: new Date(date.getFullYear(), 9, 3),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
+            },
+            {
+                name: 'Reformationstag',
+                date: new Date(date.getFullYear(), 9, 31),
+                states: ['BB', 'MV', 'SN', 'ST', 'TH']
             },
             {
                 name: 'Allerheiligen',
-                date: new Date(date.getFullYear(), 10, 1)
+                date: new Date(date.getFullYear(), 10, 1),
+                states: ['BW', 'BY', 'NW', 'RP', 'SL']
+            },
+            {
+                name: 'Buß- und Bettag',
+                date: bubeCaluclation(date),
+                states: ['SN']
             },
             {
                 name: '1. Weihnachtsfeiertag',
-                date: new Date(date.getFullYear(), 11, 25)
+                date: new Date(date.getFullYear(), 11, 25),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
             },
             {
                 name: '2. Weihnachtsfeiertag',
-                date: new Date(date.getFullYear(), 11, 26)
+                date: new Date(date.getFullYear(), 11, 26),
+                states: ['BW', 'BY', 'BE', 'BB', 'HB', 'HH', 'HE', 'MV', 'NI', 'NW', 'RP', 'SL', 'SN', 'ST', 'SH', 'TH']
             }
         ];
 
